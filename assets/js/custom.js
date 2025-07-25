c19832e78c24d894dc4d04cdb4126501bdb6243e
Md Abdul Mannan <mannan.steadfast@gmail.com>
2025-07-26 01:11:34 +0600

    Create and setup landing page

---
+++ assets/js/custom.js
@@ -0,0 +1,26 @@
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+
+(function ($) {
+  "use strict";
+
+  // Mobile Nav Hide/Show
+  if ($(".mobile-menu").length) {
+    $(".mobile-nav-toggler").on("click", function () {
+      var navMenuContent = $(".navbar-nav").html();
+      $(".mobile-menu .mobile-menu-nav .menu-outer")
+        .empty()
+        .append(navMenuContent);
+      $("body").addClass("mobile-menu-visible");
+    });
+
+    // Close mobile menu when clicking on backdrop or close button
+    $(".menu-backdrop, .mobile-menu-close").on("click", function () {
+      $("body").removeClass("mobile-menu-visible");
+    });
+  }
+})(jQuery);
